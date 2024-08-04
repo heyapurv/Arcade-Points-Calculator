c94280fd14e929841be5a2a590c2b782070fecb8
“heyapurv” <deskofapurv@gmail.com>
2024-08-04 23:15:45 +0530

    progress

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,13 +4,17 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Arcade Points Calculator",
+  title: "Points Calculator for Arcade",
   description: "A Next.js web app based on web scraping to help you calculate the Arcade Points you have earned.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en " >
+      <head>
+      <title>Points Calculator for Arcade</title>
+      <link rel="icon" href="./favicon.ico" />
+      </head>
       <body suppressHydrationWarning={true} className={inter.className} >{children}</body>
     </html>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
         <footer className="bg-slate-950  text-white">
             
             <div className="container mx-auto px-4 flex flex-col items-center justify-center  p-4">
-               <p className='text-center'>Made with ❤️ by <a href="https://www.linkedin.com/in/apurvsprofile/" className=  'hover:scale-105 text-cyan-400 duration-200'>Apurv Gore</a></p>
+               <p className='text-center'>Made with ❤️ by <a href="https://www.linkedin.com/in/apurvsprofile/" className=  ' hover:scale-110 text-cyan-400 duration-200'>Apurv Gore</a></p>
                
             </div>
         </footer>
