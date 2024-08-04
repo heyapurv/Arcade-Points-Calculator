45c6a0366beff18c40c32986b29177ff8866110e
“heyapurv” <deskofapurv@gmail.com>
2024-08-04 23:02:23 +0530

    push

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
         <footer className="bg-slate-950  text-white">
             
             <div className="container mx-auto px-4 flex flex-col items-center justify-center  p-4">
-               <p className='text-center'>Made with love by <a href="" className='text-cyan-400'>Apurv Gore</a></p>
+               <p className='text-center'>Made with ❤️ by <a href="https://www.linkedin.com/in/apurvsprofile/" className=  'hover:scale-105 text-cyan-400 duration-200'>Apurv Gore</a></p>
                
             </div>
         </footer>
